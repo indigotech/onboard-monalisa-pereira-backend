be1bced6c7881674a0c20f463109aa3a92e0e2ab
Monalisa <monakairine@gmail.com>
2021-09-17 14:43:50 -0300

    test supertest

--- src/setup.ts
+++ src/setup.ts
@@ -6,20 +6,24 @@ import { resolvers } from './schema/resolvers';
 import { typeDefs } from './schema/typedefs';
 
 export async function setupDatabase(): Promise<Connection> {
-  const connection = await createConnection();
+  const connection = await createConnection({
+    type: "postgres",
+    url: process.env.DATABASE_URL,
+  });
   console.info('Database connected');
   return connection;
 }
 
 export async function setupServer(): Promise<void> {
   const server = new ApolloServer({ resolvers, typeDefs });
-  const { url } = await server.listen();
+  const { url } = await server.listen({ port: process.env.PORT });
   console.info('running on', url);
 
 }
 
 export async function setup() {
   const path = process.env.TEST === 'true' ? `${__dirname}/../test.env` : `${__dirname}/../.env`
+  console.log(process.env.TEST, process.env.PORT)
   dotenv.config({ path })
   setupDatabase()
   setupServer();
--- src/test/index.ts
+++ src/test/index.ts
@@ -1,8 +1,25 @@
+import * as dotenv from 'dotenv';
+import { expect } from 'chai';
 import { setup } from '../setup';
 
+import supertest = require('supertest');
+
 before(async () => {
+  const path = `${__dirname}/../../test.env`;
+  dotenv.config({ path });
   await setup();
-  console.info('Test: setup done!');
 });
 
-require('./hello.test');
+
+describe('Query test', function () {
+  it('should query Hello', async () => {
+
+    const query = await supertest(`http://localhost:${process.env.PORT}`).post('/').send({
+      query: 'query { hello }'
+    });;
+    
+    expect(query.body.data.hello).to.equal('Hello World');
+    //https://www.chaijs.com/api/bdd/
+  });
+
+});
